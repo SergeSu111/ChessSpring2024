ae0b60146628ef0ef515cc7e4a31d952d43923ba
Junha <sjh666@byu.edu>
2024-05-03 15:48:00 -0600

    get rid of the King logic. Because I do not need to worry about if the next piece is king or not

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -7,8 +7,7 @@ public class BishopMovesCalculator implements PieceMovesCalculator
 {
 
     @Override
-    public  Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition)
-    {
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ChessPiece thePiece = board.getPiece(myPosition); // get the current piece
 
         return diagonal(board, thePiece, myPosition);
@@ -22,65 +21,51 @@ public class BishopMovesCalculator implements PieceMovesCalculator
 
         ArrayList<ChessMove> allBishopMoves = new ArrayList<>();
         // up right
-        for (int nextRow = startRow +1 , nextColumn = startColumn + 1;  nextRow <= 8 && nextColumn <= 8; nextRow++ , nextRow++)
-        {
+        for (int nextRow = startRow + 1, nextColumn = startColumn + 1; nextRow <= 8 && nextColumn <= 8; nextRow++, nextRow++) {
             ChessPosition nextPosition = new ChessPosition(nextRow, nextColumn); // after moving once.
-            if (bound(nextPosition)) // if it is inbound
-            {
-                ChessPiece nextPiece = board.getPiece(nextPosition); // get the current piece based on the moving position
-                if (nextPiece.getPieceType() == null)
+            ChessPiece nextPiece = board.getPiece(nextPosition); // get the current piece based on the moving position
+            if (nextPiece.getPieceType() == null) {
+                ChessMove smallMove = new ChessMove(startPosition, nextPosition, null);
+                allBishopMoves.add(smallMove); // put into big arrayList
+            } else {
+                if (nextPiece.getTeamColor() != thePiece.getTeamColor()) // if there is a piece for the next
                 {
-                    ChessMove smallMove = new ChessMove(startPosition, nextPosition, null);
-                    allBishopMoves.add(smallMove); // put into big arrayList
-                }
-                else
-                {
-                    if (nextPiece.getTeamColor() != thePiece.getTeamColor()) // if there is a piece for the next
-                    {
-                        if (nextPiece.getPieceType() == ChessPiece.PieceType.KING)  // If the next piece is King
-                        {
-                            ChessMove smallMove = new ChessMove(startPosition, nextPosition, null);
-                            allBishopMoves.add(smallMove);
-                            break;
-                        }
-                        ChessMove smallMove = new ChessMove(startPosition, nextPosition, null); // put enemy piece into my list
-                        allBishopMoves.add(smallMove);
-                    }
 
+                    ChessMove smallMove = new ChessMove(startPosition, nextPosition, null); // put enemy piece into my list
+                    allBishopMoves.add(smallMove);
                 }
+                break; // if the color is the same, just also break;
             }
-            break; // if out of bound
+
         }
 
         // up left
-        for (int nextRow = startRow + 1, nextColumn = startRow - 1; nextRow <= 8 && nextColumn >= 1; nextRow++, nextColumn--)
-        {
+        for (int nextRow = startRow + 1, nextColumn = startRow - 1; nextRow <= 8 && nextColumn >= 1; nextRow++, nextColumn--) {
             ChessPosition nextPosition = new ChessPosition(nextRow, nextColumn);
-            if (bound(nextPosition)) // if is still in bound
-            {
-                ChessPiece nextPiece = board.getPiece(nextPosition); // get the nextPiece
-                if (nextPiece.getPieceType() == null)
+            ChessPiece nextPiece = board.getPiece(nextPosition); // get the nextPiece
+            if (nextPiece.getPieceType() == null) {
+                ChessMove smallMove = new ChessMove(startPosition, nextPosition, null);
+                allBishopMoves.add(smallMove);
+            } else {
+                if (nextPiece.getTeamColor() != thePiece.getTeamColor()) // if color is different
                 {
                     ChessMove smallMove = new ChessMove(startPosition, nextPosition, null);
                     allBishopMoves.add(smallMove);
                 }
-                else
-                {
-                    if (nextPiece.getTeamColor() != thePiece.getTeamColor()) // if color is different
-                    {
-
-                    }
-                }
+                break;
             }
         }
 
         // down right
+        for (int nextRow = startRow - 1, )
 
         // down left
-    }
-
-    private Boolean bound(ChessPosition currentPosition)
-    {
-        return currentPosition.getRow() <= 8 && currentPosition.getColumn() <= 8;
+        return allBishopMoves;
     }
 }
+
+
+
+
+        // down left
+
