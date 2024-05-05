79f7f4ca851116325a3a7e9dc6825944224fd625
Junha <sjh666@byu.edu>
2024-05-05 12:33:40 -0600

    set up the colors of pawn and the startRow and NextRow and setUp row. and also finished the rowInbound method.

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class PawnMovesCalculator implements PieceMovesCalculator
@@ -13,6 +14,36 @@ public class PawnMovesCalculator implements PieceMovesCalculator
 
     public Collection<ChessMove> pawnMoves(ChessBoard board, ChessPiece thePiece, ChessPosition startPosition)
     {
-        
+        ArrayList<ChessMove> pawnMoves = new ArrayList<>();
+        // make sure color and make sure going down or going up
+        int startRow = startPosition.getRow();
+        int startColumn = startPosition.getColumn();
+        int nextRow;
+
+        // make sure if its empty in front of the pawn
+        boolean empty = false;
+
+        // set setup row and column
+        int setUpRow;
+
+        // means going up.
+        if (thePiece.getTeamColor() == ChessGame.TeamColor.WHITE)
+        {
+            setUpRow = 2;
+            nextRow = startRow + 1;
+        }
+        // else is Black
+        setUpRow = 7;
+        nextRow = startRow - 1;
+
+
+
+    }
+
+
+    // create rowInBound
+    public boolean rowInBound(int row)
+    {
+        return row >= 1 && row <= 8;
     }
 }
