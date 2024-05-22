99d9425bda2dab409f511ed1c0d85960464256f5
Junha <sjh666@byu.edu>
2024-05-22 11:04:46 -0600

    created those handler classes

---
+++ server/src/main/java/Handlers/baseHandler.java
@@ -0,0 +1,7 @@
+package Handlers;
+
+public class baseHandler {
+    // if the web API requires an authToken, write the logic to valid the authToken in base handler class
+
+
+}
---
+++ server/src/main/java/Handlers/registerHandler.java
@@ -0,0 +1,8 @@
+package Handlers;
+
+public class registerHandler extends baseHandler
+{
+    // make the json request to be java request objects
+    // call the service function and return
+    // return as json
+}
