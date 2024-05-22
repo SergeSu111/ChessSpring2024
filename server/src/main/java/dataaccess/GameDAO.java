4161f5ba145d0a02f001b13919ea45ba10b30e21
Junha <sjh666@byu.edu>
2024-05-22 17:41:52 -0600

    finished the GameDAO. But I am not sure if I need to write the joinGame.

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,34 @@
 package dataaccess;
 
+import Model.GameData;
+import chess.ChessGame;
+
+import java.util.ArrayList;
+
 public interface GameDAO {
+    public default String createGame(String gameName) throws DataAccessException {
+        return null;
+    }
+
+    public default GameData getGame(ChessGame.TeamColor playerColor, int gameID) throws DataAccessException {
+        return null;
+    }
+
+    public default ArrayList<ChessGame> listGames(String authToken) throws DataAccessException
+    {
+        return null;
+    }
+
+    public default void updateGame(String username, ChessGame.TeamColor playerColor, String gameID) throws DataAccessException
+    {
+        return;
+    }
+
+    public default void clear() throws DataAccessException
+    {
+        return;
+    }
+
+
+
 }
