ecc2d3adc5f795b27d685f41d61830c47637e788
Junha <sjh666@byu.edu>
2024-05-22 16:04:46 -0600

    get rid of some unuseful requests and responses. And create the OnlyMessageResponse to instead of the clear join and logout response

--- server/src/main/java/Handlers/registerHandler.java
+++ server/src/main/java/Handlers/registerHandler.java
@@ -1,8 +1,20 @@
 package Handlers;
 
+import HttpRequest.RegisterRequest;
+import org.eclipse.jetty.server.Response;
+import spark.Request;
+
 public class registerHandler extends baseHandler
 {
-    // make the json request to be java request objects
-    // call the service function and return
+    @Override
+    public Object httpHandlerRequest(Request request, Response response)
+    {
+        // make the json request to be java request objects
+        RegisterRequest body = getBody(request, RegisterRequest.class); // change the request to java object
+
+        return null;
+    }
+
+
     // return as json
 }
--- server/src/main/java/HttpRequest/ClearRequest.java
+++
@@ -1,4 +0,0 @@
-package HttpRequest;
-
-public record ClearRequest() {
-}
