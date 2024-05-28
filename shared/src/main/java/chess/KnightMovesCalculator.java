e9097fc1517af19479e97f49f5566a5855138ac6
Junha <sjh666@byu.edu>
2024-05-28 17:36:59 -0600

    fix some duplicated code

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -12,6 +12,29 @@ public class KnightMovesCalculator implements PieceMovesCalculator
         return knightMove(board, thePiece, myPosition);
     }
 
+    private void addingToMoves(int nextRow, int nextColumn, ChessPosition startPosition, ChessBoard board, ArrayList<ChessMove> knightMoves, ChessPiece thePiece)
+    {
+        if (isInBound(new ChessPosition(nextRow, nextColumn)))
+        {
+            ChessPosition endPosition = new ChessPosition(nextRow, nextColumn); // get the end Position
+            ChessPiece endPiece = board.getPiece(endPosition); // get the piece
+            if (endPiece == null)
+            {
+                ChessMove smallMove = new ChessMove(startPosition, endPosition, null);
+                knightMoves.add(smallMove);
+            }
+            else
+            {
+                if (endPiece.getTeamColor() != thePiece.getTeamColor())
+                {
+                    ChessMove smallMove = new ChessMove(startPosition, endPosition, null);
+                    knightMoves.add(smallMove);
+                }
+            }
+        }
+
+
+    }
     private Collection<ChessMove> knightMove(ChessBoard board, ChessPiece thePiece, ChessPosition startPosition) {
         int startColumn = startPosition.getColumn();
         int startRow = startPosition.getRow();
@@ -22,175 +45,47 @@ public class KnightMovesCalculator implements PieceMovesCalculator
         // right 1 up 2
         nextRow = startRow + 2;
         nextColumn = startColumn + 1;
-        if (isInBound(new ChessPosition(nextRow, nextColumn)))  // if the endPosition is inBound
-        {
-            ChessPosition endPosition = new ChessPosition(nextRow, nextColumn); // get the end Position
-            ChessPiece endPiece = board.getPiece(endPosition); // get the piece
-            if (endPiece == null)
-            {
-                ChessMove smallMove = new ChessMove(startPosition, endPosition, null);
-                knightMoves.add(smallMove);
-            }
-            else
-            {
-                if (endPiece.getTeamColor() != thePiece.getTeamColor())
-                {
-                    ChessMove smallMove = new ChessMove(startPosition, endPosition, null);
-                    knightMoves.add(smallMove);
-                }
-            }
-        }
+        addingToMoves(nextRow, nextColumn, startPosition, board, knightMoves, thePiece);
+
 
 
         // left 1 up 2
         nextColumn = startColumn - 1;
         nextRow = startRow + 2;
-        if (isInBound(new ChessPosition(nextRow, nextColumn)))  // if the endPosition is inBound
-        {
-            ChessPosition endPosition = new ChessPosition(nextRow, nextColumn); // get the end Position
-            ChessPiece endPiece = board.getPiece(endPosition); // get the piece
-            if (endPiece == null)
-            {
-                ChessMove smallMove = new ChessMove(startPosition, endPosition, null);
-                knightMoves.add(smallMove);
-            }
-            else
-            {
-                if (endPiece.getTeamColor() != thePiece.getTeamColor())
-                {
-                    ChessMove smallMove = new ChessMove(startPosition, endPosition, null);
-                    knightMoves.add(smallMove);
-                }
-            }
-        }
+        addingToMoves(nextRow, nextColumn, startPosition, board, knightMoves, thePiece);
+
 
         // right 2 up 1
         nextColumn = startColumn + 2;
         nextRow = startRow + 1;
-        if (isInBound(new ChessPosition(nextRow, nextColumn)))  // if the endPosition is inBound
-        {
-            ChessPosition endPosition = new ChessPosition(nextRow, nextColumn); // get the end Position
-            ChessPiece endPiece = board.getPiece(endPosition); // get the piece
-            if (endPiece == null)
-            {
-                ChessMove smallMove = new ChessMove(startPosition, endPosition, null);
-                knightMoves.add(smallMove);
-            }
-            else
-            {
-                if (endPiece.getTeamColor() != thePiece.getTeamColor())
-                {
-                    ChessMove smallMove = new ChessMove(startPosition, endPosition, null);
-                    knightMoves.add(smallMove);
-                }
-            }
-        }
+        addingToMoves(nextRow, nextColumn, startPosition, board, knightMoves, thePiece);
+
 
         // left 2 up 1
         nextColumn = startColumn - 2;
         nextRow = startRow + 1;
-        if (isInBound(new ChessPosition(nextRow, nextColumn)))  // if the endPosition is inBound
-        {
-            ChessPosition endPosition = new ChessPosition(nextRow, nextColumn); // get the end Position
-            ChessPiece endPiece = board.getPiece(endPosition); // get the piece
-            if (endPiece == null)
-            {
-                ChessMove smallMove = new ChessMove(startPosition, endPosition, null);
-                knightMoves.add(smallMove);
-            }
-            else
-            {
-                if (endPiece.getTeamColor() != thePiece.getTeamColor())
-                {
-                    ChessMove smallMove = new ChessMove(startPosition, endPosition, null);
-                    knightMoves.add(smallMove);
-                }
-            }
-        }
+        addingToMoves(nextRow, nextColumn, startPosition, board, knightMoves, thePiece);
+
         // right 1 down 2
         nextColumn = startColumn + 1;
         nextRow = startRow - 2;
-        if (isInBound(new ChessPosition(nextRow, nextColumn)))  // if the endPosition is inBound
-        {
-            ChessPosition endPosition = new ChessPosition(nextRow, nextColumn); // get the end Position
-            ChessPiece endPiece = board.getPiece(endPosition); // get the piece
-            if (endPiece == null)
-            {
-                ChessMove smallMove = new ChessMove(startPosition, endPosition, null);
-                knightMoves.add(smallMove);
-            }
-            else
-            {
-                if (endPiece.getTeamColor() != thePiece.getTeamColor())
-                {
-                    ChessMove smallMove = new ChessMove(startPosition, endPosition, null);
-                    knightMoves.add(smallMove);
-                }
-            }
-        }
+        addingToMoves(nextRow, nextColumn, startPosition, board, knightMoves, thePiece);
+
         // left 1 down 2
         nextColumn = startColumn - 1;
         nextRow = startRow - 2;
-        if (isInBound(new ChessPosition(nextRow, nextColumn)))  // if the endPosition is inBound
-        {
-            ChessPosition endPosition = new ChessPosition(nextRow, nextColumn); // get the end Position
-            ChessPiece endPiece = board.getPiece(endPosition); // get the piece
-            if (endPiece == null)
-            {
-                ChessMove smallMove = new ChessMove(startPosition, endPosition, null);
-                knightMoves.add(smallMove);
-            }
-            else
-            {
-                if (endPiece.getTeamColor() != thePiece.getTeamColor())
-                {
-                    ChessMove smallMove = new ChessMove(startPosition, endPosition, null);
-                    knightMoves.add(smallMove);
-                }
-            }
-        }
+        addingToMoves(nextRow, nextColumn, startPosition, board, knightMoves, thePiece);
+
         // right 2 down 1
         nextColumn = startColumn + 2;
         nextRow = startRow - 1;
-        if (isInBound(new ChessPosition(nextRow, nextColumn)))  // if the endPosition is inBound
-        {
-            ChessPosition endPosition = new ChessPosition(nextRow, nextColumn); // get the end Position
-            ChessPiece endPiece = board.getPiece(endPosition); // get the piece
-            if (endPiece == null)
-            {
-                ChessMove smallMove = new ChessMove(startPosition, endPosition, null);
-                knightMoves.add(smallMove);
-            }
-            else
-            {
-                if (endPiece.getTeamColor() != thePiece.getTeamColor())
-                {
-                    ChessMove smallMove = new ChessMove(startPosition, endPosition, null);
-                    knightMoves.add(smallMove);
-                }
-            }
-        }
+        addingToMoves(nextRow, nextColumn, startPosition, board, knightMoves, thePiece);
+
         // left 2 down 1
         nextColumn = startColumn - 2;
         nextRow = startRow - 1;
-        if (isInBound(new ChessPosition(nextRow, nextColumn)))  // if the endPosition is inBound
-        {
-            ChessPosition endPosition = new ChessPosition(nextRow, nextColumn); // get the end Position
-            ChessPiece endPiece = board.getPiece(endPosition); // get the piece
-            if (endPiece == null)
-            {
-                ChessMove smallMove = new ChessMove(startPosition, endPosition, null);
-                knightMoves.add(smallMove);
-            }
-            else
-            {
-                if (endPiece.getTeamColor() != thePiece.getTeamColor())
-                {
-                    ChessMove smallMove = new ChessMove(startPosition, endPosition, null);
-                    knightMoves.add(smallMove);
-                }
-            }
-        }
+        addingToMoves(nextRow, nextColumn, startPosition, board, knightMoves, thePiece);
+
         return knightMoves;
 
     }
