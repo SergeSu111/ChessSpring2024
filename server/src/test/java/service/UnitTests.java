5e6d84c2625183fe10c37b75206038f77ffc8d11
Junha <sjh666@byu.edu>
2024-05-28 15:31:05 -0600

    finished the loginSuccess. And fixed the registerfailed test. Because the registered username and email are same with registerSuccess. So it just occur dataccess exception.

--- server/src/test/java/service/UnitTests.java
+++ server/src/test/java/service/UnitTests.java
@@ -1,6 +1,8 @@
 package service;
 
+import HttpRequest.LoginRequest;
 import HttpRequest.RegisterRequest;
+import HttpResponse.LoginResponse;
 import HttpResponse.RegisterResponse;
 import dataaccess.*;
 import org.junit.jupiter.api.*;
@@ -14,6 +16,7 @@ public class UnitTests
 
     private final RegisterRequest registerRequestTest = new RegisterRequest("Serge", "Serge666", "sjh666@byu.edu");
 
+    private final LoginRequest loginRequest = new LoginRequest("Serge", "Serge666");
     private final MemoryAuthDAO authDAO = new MemoryAuthDAO();
 
     private final MemoryUserDAO userDAO = new MemoryUserDAO();
@@ -21,6 +24,7 @@ public class UnitTests
     private final MemoryGameDAO gameDAO = new MemoryGameDAO();
 
     private final registerService registerServiceTest = new registerService();
+    private final loginService loginServiceTest = new loginService();
 
 
     @Test
@@ -34,8 +38,6 @@ public class UnitTests
     @Order(2)
     public void registerSuccess() throws ServerException, ClientException, DataAccessException {
 
-
-
         RegisterResponse registerResponseTest = registerServiceTest.register(registerRequestTest);
         String username = registerResponseTest.username();
 
@@ -45,11 +47,21 @@ public class UnitTests
     @Test
     @Order(3)
     public void registerFailed() throws ServerException, ClientException, DataAccessException {
-        RegisterRequest missingPassword = new RegisterRequest("Serge", null, "sjh666@byu.edu");
+        RegisterRequest missingPassword = new RegisterRequest("lala", null, "sjh66@byu.edu");
         ClientException clientException = assertThrows(ClientException.class, () ->  registerServiceTest.register(missingPassword));
         assertEquals(clientException.getMessage(), "Error: bad request");
-
     }
+
+    @Test
+    @Order(4)
+    public void loginSuccess() throws ServerException, DataAccessException, ClientException {
+       RegisterResponse registerResponseTest = registerServiceTest.register(new RegisterRequest("Su", "123", "Junhao.S@Outlook.com"));
+       LoginResponse loginResponse = loginServiceTest.login(new LoginRequest("Su", "123"));
+       String username = registerResponseTest.username();
+       assertEquals(username, loginResponse.username());
+    }
+
+
 
 
 }
