411bd7203b6c40dae5f877d106c1b5e32a96c4aa
Junha <sjh666@byu.edu>
2024-05-28 18:51:12 -0600

    rewrite and re-construct the pawnMove function to make it shorter

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -17,117 +17,69 @@ public class PawnMovesCalculator implements PieceMovesCalculator
         return "PawnMovesCalculator{}";
     }
 
-    public Collection<ChessMove> pawnMoves(ChessBoard board, ChessPiece thePiece, ChessPosition startPosition)
-    {
+    public Collection<ChessMove> pawnMoves(ChessBoard board, ChessPiece thePiece, ChessPosition startPosition) {
         ArrayList<ChessMove> pawnMoves = new ArrayList<>();
         int startRow = startPosition.getRow();
         int startColumn = startPosition.getColumn();
         int nextRow, setUpRow;
-        if (thePiece.getTeamColor() == ChessGame.TeamColor.WHITE)
-        {
+
+        if (thePiece.getTeamColor() == ChessGame.TeamColor.WHITE) {
             setUpRow = 2;
             nextRow = startRow + 1;
-        }
-        else
-        {
+        } else {
             setUpRow = 7;
             nextRow = startRow - 1;
         }
-        if (rowInBound(nextRow))
-        {
-            ChessPiece nextPiece = board.getPiece(new ChessPosition(nextRow, startColumn)); // get the nextPiece
-            if (nextPiece == null) // if nextPiece is null
-            {
-                if (nextRow == 1 || nextRow == 8)  // means need to promote
-                {
-                    pawnMoves.add(new ChessMove(startPosition, new ChessPosition(nextRow, startColumn), ChessPiece.PieceType.ROOK));
-                    pawnMoves.add(new ChessMove(startPosition, new ChessPosition(nextRow, startColumn), ChessPiece.PieceType.BISHOP));
-                    pawnMoves.add(new ChessMove(startPosition, new ChessPosition(nextRow, startColumn), ChessPiece.PieceType.KNIGHT));
-                    pawnMoves.add(new ChessMove(startPosition, new ChessPosition(nextRow, startColumn), ChessPiece.PieceType.QUEEN));
-                }
-                else
-                {
-                    pawnMoves.add( new ChessMove(startPosition, new ChessPosition(nextRow, startColumn), null));
-                }
-            }
-        }
-        if (startRow == setUpRow)
-        {
-            if (thePiece.getTeamColor() == ChessGame.TeamColor.WHITE)
-            {
-                int nextNextRow = nextRow + 1;  // just in case you have to make sure the nextRow has not same color pieces
-                ChessPosition nextNextPosition = new ChessPosition(nextNextRow, startColumn);
-                ChessPiece nextNextPiece = board.getPiece(nextNextPosition);
-                ChessPosition nextPosition = new ChessPosition(nextRow, startColumn);
-                ChessPiece nextPiece = board.getPiece(nextPosition);
-                if (nextNextPiece == null && nextPiece == null)
-                {
-                    pawnMoves.add( new ChessMove(startPosition, nextNextPosition, null));
-                }
-                if (nextPiece == null && nextNextPiece != null)  // if nextPiece is null but nextNext is not . I can go 1 step even if I am in the setup Line
-                {
-                    pawnMoves.add( new ChessMove(startPosition, nextPosition, null));
-                }
-            }
-            else
-            {
-                int nextNextRow = nextRow - 1;  // just in case you have to make sure the nextRow has not same color pieces
-                ChessPosition nextNextPosition = new ChessPosition(nextNextRow, startColumn);
-                ChessPiece nextNextPiece = board.getPiece(nextNextPosition);
-                ChessPosition nextPosition = new ChessPosition(nextRow, startColumn);
-                ChessPiece nextPiece = board.getPiece(nextPosition);
-                if (nextNextPiece == null && nextPiece == null)
-                {
-                    pawnMoves.add(new ChessMove(startPosition, nextNextPosition, null));
-                }
-                if (nextPiece == null && nextNextPiece != null)  // if nextPiece is null but nextNext is not . I can go 1 step even if I am in the setup Line
-                {
-                    pawnMoves.add(new ChessMove(startPosition, nextPosition, null));
-                }
-            }
-        }
-        int nextColumn = startColumn + 1;
-        ChessPosition nextPositionRight = new ChessPosition(nextRow, nextColumn); // 右斜方的位置
-        if (KnightMovesCalculator.isInBound(nextPositionRight)) // if the nextPosition is in bound
-        {
-            ChessPiece nextPiece = board.getPiece(nextPositionRight);
-            if (nextPiece != null && nextPiece.getTeamColor() != thePiece.getTeamColor())  // I can eat
-            {
-                if (nextPositionRight.getRow() == 1 || nextPositionRight.getRow() == 8) // needs promote
-                {
-                    pawnMoves.add(new ChessMove(startPosition, nextPositionRight, ChessPiece.PieceType.ROOK));
-                    pawnMoves.add(new ChessMove(startPosition, nextPositionRight, ChessPiece.PieceType.QUEEN));
-                    pawnMoves.add(new ChessMove(startPosition, nextPositionRight, ChessPiece.PieceType.KNIGHT));
-                    pawnMoves.add(new ChessMove(startPosition, nextPositionRight, ChessPiece.PieceType.BISHOP));
-                }
-                else
-                {
-                    pawnMoves.add(new ChessMove(startPosition, nextPositionRight, null));
-                }
-            }
-        }
-        nextColumn -= 2; // because we just got the right. we want to turned it left. -2
-        ChessPosition nextPosition = new ChessPosition(nextRow, nextColumn); // 右斜方的位置
-        if (KnightMovesCalculator.isInBound(nextPosition)) // if the nextPosition is in bound
-        {
-            ChessPiece nextPieceLeft = board.getPiece(nextPosition);
-            if (nextPieceLeft != null && nextPieceLeft.getTeamColor() != thePiece.getTeamColor())  // I can eat
-            {
-                if (nextPosition.getRow() == 1 || nextPosition.getRow() == 8) // needs promote
-                {
-                    pawnMoves.add(new ChessMove(startPosition, nextPosition, ChessPiece.PieceType.ROOK));
-                    pawnMoves.add(new ChessMove(startPosition, nextPosition, ChessPiece.PieceType.QUEEN));
-                    pawnMoves.add(new ChessMove(startPosition, nextPosition, ChessPiece.PieceType.KNIGHT));
-                    pawnMoves.add(new ChessMove(startPosition, nextPosition, ChessPiece.PieceType.BISHOP));
-                }
-                else
-                {
-                    pawnMoves.add(new ChessMove(startPosition, nextPosition, null));
-                }
-            }
-        }
+
+        addForwardMoves(board, thePiece, startPosition, startRow, startColumn, nextRow, setUpRow, pawnMoves);
+        addCaptureMoves(board, thePiece, startPosition, startRow, startColumn, nextRow, pawnMoves);
+
         return pawnMoves;
     }
+
+    private void addForwardMoves(ChessBoard board, ChessPiece thePiece, ChessPosition startPosition, int startRow, int startColumn, int nextRow, int setUpRow, ArrayList<ChessMove> pawnMoves) {
+        if (rowInBound(nextRow) && board.getPiece(new ChessPosition(nextRow, startColumn)) == null) {
+            if (nextRow == 1 || nextRow == 8) {
+                addPromotionMoves(startPosition, nextRow, startColumn, pawnMoves);
+            } else {
+                pawnMoves.add(new ChessMove(startPosition, new ChessPosition(nextRow, startColumn), null));
+            }
+
+            if (startRow == setUpRow) {
+                int nextNextRow = thePiece.getTeamColor() == ChessGame.TeamColor.WHITE ? nextRow + 1 : nextRow - 1;
+                if (rowInBound(nextNextRow) && board.getPiece(new ChessPosition(nextNextRow, startColumn)) == null) {
+                    pawnMoves.add(new ChessMove(startPosition, new ChessPosition(nextNextRow, startColumn), null));
+                }
+            }
+        }
+    }
+
+    private void addCaptureMoves(ChessBoard board, ChessPiece thePiece, ChessPosition startPosition, int startRow, int startColumn, int nextRow, ArrayList<ChessMove> pawnMoves) {
+        int[] captureColumns = {startColumn + 1, startColumn - 1};
+        for (int captureColumn : captureColumns) {
+            ChessPosition capturePosition = new ChessPosition(nextRow, captureColumn);
+            if (rowInBound(captureColumn) && board.getPiece(capturePosition) != null && board.getPiece(capturePosition).getTeamColor() != thePiece.getTeamColor()) {
+                if (nextRow == 1 || nextRow == 8) {
+                    addPromotionMoves(startPosition, nextRow, captureColumn, pawnMoves);
+                } else {
+                    pawnMoves.add(new ChessMove(startPosition, capturePosition, null));
+                }
+            }
+        }
+    }
+
+    private void addPromotionMoves(ChessPosition startPosition, int row, int column, ArrayList<ChessMove> pawnMoves) {
+        ChessPiece.PieceType[] promotionPieces = {
+                ChessPiece.PieceType.ROOK,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.QUEEN
+        };
+        for (ChessPiece.PieceType pieceType : promotionPieces) {
+            pawnMoves.add(new ChessMove(startPosition, new ChessPosition(row, column), pieceType));
+        }
+    }
+
     public boolean rowInBound(int row)
     {
         return row >= 1 && row <= 8;
