28e5d91d970a0a955343ed3011e391cfa4606cef
Junha <sjh666@byu.edu>
2024-05-28 18:06:19 -0600

    fix some code quality

--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -12,6 +12,27 @@ public class RookMovesCalculator implements PieceMovesCalculator
         return straight(board, thePiece, myPosition);
     }
 
+    public static boolean addingMove(ChessBoard board, ChessPiece thePiece, int nextRow, int nextColumn, ArrayList<ChessMove> rookMoves, ChessPosition startPosition)
+    {
+        ChessPosition nextPosition = new ChessPosition(nextRow, nextColumn); // get the nextPosition
+        ChessPiece nextPiece = board.getPiece(nextPosition);
+        if (nextPiece == null)
+        {
+            ChessMove smallMove = new ChessMove(startPosition, nextPosition, null);
+            rookMoves.add(smallMove);
+            return false;
+        }
+        else
+        {
+            if (nextPiece.getTeamColor() != thePiece.getTeamColor())
+            {
+                ChessMove smallMove = new ChessMove(startPosition, nextPosition, null); // if Color is different, enemy's piece get it.
+                rookMoves.add(smallMove);
+                return true;
+            }
+            return true;
+        }
+    }
     public Collection<ChessMove> straight(ChessBoard board, ChessPiece thePiece, ChessPosition startPosition)
     {
         ArrayList<ChessMove> rookMoves = new ArrayList<>();
@@ -20,20 +41,8 @@ public class RookMovesCalculator implements PieceMovesCalculator
         // up
         for (int nextRow = startRow + 1; nextRow <= 8; nextRow++)
         {
-            ChessPosition nextPosition = new ChessPosition(nextRow, startColumn); // get the nextPosition
-            ChessPiece nextPiece = board.getPiece(nextPosition);
-            if (nextPiece == null)
+            if (addingMove(board, thePiece, nextRow, startColumn, rookMoves, startPosition))
             {
-                ChessMove smallMove = new ChessMove(startPosition, nextPosition, null);
-                rookMoves.add(smallMove);
-            }
-            else
-            {
-                if (nextPiece.getTeamColor() != thePiece.getTeamColor())
-                {
-                    ChessMove smallMove = new ChessMove(startPosition, nextPosition, null); // if Color is different, enemy's piece get it.
-                    rookMoves.add(smallMove);
-                }
                 break;
             }
             if(thePiece.getPieceType() == ChessPiece.PieceType.KING)
@@ -44,20 +53,8 @@ public class RookMovesCalculator implements PieceMovesCalculator
         // down
         for (int nextRow = startRow - 1; nextRow >= 1; nextRow--)
         {
-            ChessPosition nextPosition = new ChessPosition(nextRow, startColumn); // get the nextPosition
-            ChessPiece nextPiece = board.getPiece(nextPosition);
-            if (nextPiece == null)
+            if (addingMove(board, thePiece, nextRow, startColumn, rookMoves, startPosition))
             {
-                ChessMove smallMove = new ChessMove(startPosition, nextPosition, null);
-                rookMoves.add(smallMove);
-            }
-            else
-            {
-                if (nextPiece.getTeamColor() != thePiece.getTeamColor())
-                {
-                    ChessMove smallMove = new ChessMove(startPosition, nextPosition, null); // if Color is different, enemy's piece get it.
-                    rookMoves.add(smallMove);
-                }
                 break;
             }
             if(thePiece.getPieceType() == ChessPiece.PieceType.KING)
@@ -68,21 +65,8 @@ public class RookMovesCalculator implements PieceMovesCalculator
         // right
         for (int nextColumn = startColumn + 1; nextColumn <= 8; nextColumn++)
         {
-            ChessPosition nextPosition = new ChessPosition(startRow, nextColumn); // get the nextPosition
-            ChessPiece nextPiece = board.getPiece(nextPosition);
-            if (nextPiece == null)
+            if (addingMove(board, thePiece, startRow, nextColumn, rookMoves, startPosition))
             {
-                ChessMove smallMove = new ChessMove(startPosition, nextPosition, null);
-                rookMoves.add(smallMove);
-
-            }
-            else
-            {
-                if (nextPiece.getTeamColor() != thePiece.getTeamColor())
-                {
-                    ChessMove smallMove = new ChessMove(startPosition, nextPosition, null); // if Color is different, enemy's piece get it.
-                    rookMoves.add(smallMove);
-                }
                 break;
             }
             if(thePiece.getPieceType() == ChessPiece.PieceType.KING)
@@ -93,20 +77,8 @@ public class RookMovesCalculator implements PieceMovesCalculator
         // left
         for (int nextColumn = startColumn - 1; nextColumn >= 1; nextColumn--)
         {
-            ChessPosition nextPosition = new ChessPosition(startRow, nextColumn); // get the nextPosition
-            ChessPiece nextPiece = board.getPiece(nextPosition);
-            if (nextPiece == null)
+            if (addingMove(board, thePiece, startRow, nextColumn, rookMoves, startPosition))
             {
-                ChessMove smallMove = new ChessMove(startPosition, nextPosition, null);
-                rookMoves.add(smallMove);
-            }
-            else
-            {
-                if (nextPiece.getTeamColor() != thePiece.getTeamColor())
-                {
-                    ChessMove smallMove = new ChessMove(startPosition, nextPosition, null); // if Color is different, enemy's piece get it.
-                    rookMoves.add(smallMove);
-                }
                 break;
             }
             if(thePiece.getPieceType() == ChessPiece.PieceType.KING)
