9f490ae68deeda3887ec1a350aad95ad4f96ff30
Junha <sjh666@byu.edu>
2024-05-30 17:42:41 -0600

    followed with Dr.Wilkerson said, create a hashedPassword class to hash and check it.

--- server/src/main/java/dataaccess/HashedPassword.java
+++ server/src/main/java/dataaccess/HashedPassword.java
@@ -1,6 +1,18 @@
 package dataaccess;
-
+import org.mindrot.jbcrypt.BCrypt;
 public class HashedPassword
 {
+    private static String normalPassword;
+
+    public static String hashPassword(String normalPassword)
+    {
+        return BCrypt.hashpw(normalPassword, BCrypt.gensalt());
+    }
+
+    public static boolean checkPassWord(String normalPassword)
+    {
+        String hashedPassword = hashPassword(normalPassword);
+        return BCrypt.checkpw(normalPassword, hashedPassword);
+    }
 
 }
