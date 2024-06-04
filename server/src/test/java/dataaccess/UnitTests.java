3f1535371f9c87ae3ca2f164b6e27ce813ef9b58
Junha <sjh666@byu.edu>
2024-06-03 23:20:51 -0600

    passed register and login success and failed tests.

--- server/src/test/java/dataaccess/UnitTests.java
+++ server/src/test/java/dataaccess/UnitTests.java
@@ -1,16 +1,74 @@
 package dataaccess;
 
+import model.UserData;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import dataaccess.DatabaseManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UnitTests {
 
     // before each test we can clean up all tables to not affect other tests
 
+    private final sqlUser sqlUserRefer = new sqlUser();
+    private final sqlAuth sqlAuthRefer = new sqlAuth();
+    private final sqlGame sqlGameRefer = new sqlGame();
+
+    private final UserData userData = new UserData("Serge", "Serge111", "sjh66@byu.edu");
+    private final UserData userDataFailed = new UserData("", "Serge", "sjh66@byu.edu");
+
+    public UnitTests() throws DataAccessException {
+    }
+
+    @BeforeEach
+    public void createDB() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        sqlUser.createUserTable();
+        sqlAuth.createAuthTable();
+        sqlGame.createGamesTable();
+
+
+    }
 
     @BeforeEach
     @Test
-    public void clear()
-    {
+    public void clear() throws DataAccessException {
+        sqlUserRefer.clear();
+        sqlAuthRefer.clear();
+        sqlGameRefer.clear();
+        assertDoesNotThrow(sqlUserRefer::clear);
+        assertDoesNotThrow(sqlAuthRefer::clear);
+        assertDoesNotThrow(sqlGameRefer::clear);
+    }
 
+    @Test
+    @Order(1)
+    public void registerSuccess() throws DataAccessException {
+        assertDoesNotThrow(() -> sqlUserRefer.createUser(userData));
+    }
+
+    @Test
+    @Order(2)
+    public void registerFailed() throws DataAccessException {
+        sqlUserRefer.createUser(userData);
+        assertThrows(DataAccessException.class, () -> sqlUserRefer.createUser(userData));
+    }
+
+    @Test
+    @Order(3)
+    public void loginSuccess() throws DataAccessException {
+        sqlUserRefer.createUser(userData);
+        UserData returnedUserData = sqlUserRefer.getUser(userData.username());
+        assertEquals(userData.username(), returnedUserData.username());
+    }
+
+    @Test
+    @Order(4)
+    public void loginFailed() throws DataAccessException {
+        sqlUserRefer.createUser(userData);
+        UserData returnedUserData = sqlUserRefer.getUser(userData.username());
+        assertNotEquals(returnedUserData, userDataFailed);
     }
 }
