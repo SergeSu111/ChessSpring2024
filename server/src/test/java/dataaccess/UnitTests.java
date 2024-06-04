d991e5e512df5639c56b480febdcb6b185b4179e
Junha <sjh666@byu.edu>
2024-06-03 21:26:54 -0600

    create UnitTests, start to work on it

---
+++ server/src/test/java/dataaccess/UnitTests.java
@@ -0,0 +1,16 @@
+package dataaccess;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+public class UnitTests {
+
+    // before each test we can clean up all tables to not affect other tests
+
+
+    @BeforeEach
+    @Test
+    public void clear()
+    {
+
+    }
+}
