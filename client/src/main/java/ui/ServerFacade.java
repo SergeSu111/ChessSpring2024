091dfc041f34a53d4b69ba3c677f37fb31be1422
Junha <sjh666@byu.edu>
2024-06-05 18:39:51 -0600

    finished the getResponseFromHandlers method. But I got a little confused if returned a error message. How I handle this

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -29,7 +29,9 @@ public class ServerFacade{
         URL uri = new URL(RegisterURL);
         String method = "POST";
         HttpURLConnection http = sendRequest(uri, registerRequest, method);
-
+        // how about return a error message? still registerResponse
+        RegisterResponse registerResponse = (RegisterResponse) getResponseFromHandlers(http);
+        
 
 
     }
@@ -42,8 +44,7 @@ public class ServerFacade{
         http.setRequestMethod(method);
         String jsonRequestBody = gson.toJson(RequestBody); // make it as json before putting into body
         writeRequestBody(jsonRequestBody, http);
-
-
+        return http; // return the http that already had the request
     }
 
     // write body
@@ -60,15 +61,21 @@ public class ServerFacade{
     // get response
     private static Object getResponseFromHandlers(HttpURLConnection http) throws IOException
     {
-        Gson gson = new Gson();
         Object responseBody;
-        try (InputStream respBody = http.getInputStream())
+        Gson gson = new Gson();
+        if (http.getResponseCode() == 200)
         {
-            InputStreamReader inputStreamReader = new InputStreamReader(respBody);
-            responseBody = gson.fromJson(inputStreamReader, Map.class);
+            try (InputStream respBody = http.getInputStream())
+            {
+                InputStreamReader inputStreamReader = new InputStreamReader(respBody);
+                responseBody = gson.fromJson(inputStreamReader, Map.class);
+            }
+        }
+        else
+        {
+            responseBody = http.getErrorStream();
         }
         return responseBody;
-
     }
 
 }
