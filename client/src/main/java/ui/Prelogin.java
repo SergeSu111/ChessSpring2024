e4a45507382929547fbbb1a009db9b9e26d00ce7
Junha <sjh666@byu.edu>
2024-06-06 22:12:35 -0600

    init the Prelogin class,and changed the Server in main of client with Prelogin. Run the Main in client, got the console. Still need to work on the run method in prelogin

--- client/src/main/java/ui/Prelogin.java
+++ client/src/main/java/ui/Prelogin.java
@@ -1,4 +1,27 @@
 package ui;
 
-public class Prelogin {
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import static ui.EscapeSequences.BLACK_KING;
+
+public class Prelogin
+{
+    private static final PrintStream out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
+    private final ServerFacade serverFacade;
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public Prelogin(String serverUrl)
+    {
+        serverFacade = new ServerFacade(serverUrl);
+    }
+
+    public void run()
+    {
+        out.println(STR."\{BLACK_KING}Welcome to the Chess Game. Type Here to get started.\{BLACK_KING}");
+    }
+
 }
