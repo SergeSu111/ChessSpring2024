d013756f67aece86b85d7b1d7b8185d082b9e077
Junha <sjh666@byu.edu>
2024-06-11 19:29:54 -0600

    working on how the message got from front end can match what the methods I gonna write in switch. Go for dinner, do it later maybe tonight

--- server/src/main/java/WebsocketHandler.java
+++ server/src/main/java/WebsocketHandler.java
@@ -1,18 +1,40 @@
+import chess.ChessGame;
+import com.google.gson.Gson;
+import com.google.protobuf.Enum;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+import java.security.Key;
 
 @WebSocket
 public class WebsocketHandler
 {
     // websocket back-end. Receiving messages from websocket facade. return the messages from it to send back to websocketFacade.
     private Connection connection;
+    private String keyItem;
     private final ConnectionManager connectionManager = new ConnectionManager();
 
+    public enum KeyItems {join, observe, move, leave, resign, check, checkmate}
     @OnWebSocketMessage
     public void onMessage(Session session, String message)
     {
-
+        for (var item : KeyItems.values())
+        {
+            String itemJson = item.toString();
+            if (message.contains(itemJson))
+            {
+                keyItem = itemJson;
+            }
+        }
+        Gson gson = new Gson();
+        KeyItems itemBack = gson.fromJson(keyItem, KeyItems.class);
+        switch (itemBack)
+        {
+            case itemBack == KeyItems.join -> joinGame();
+            case itemBack == KeyItems.observe -> observeGame();
+            case
+        }
     }
 
 }
