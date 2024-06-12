378109afbf0abbe24a010f0ef03e048dcb99e08b
Junha <sjh666@byu.edu>
2024-06-11 19:03:28 -0600

    tried to work on WebsocketHandler by imitating the pet shop's

--- server/src/main/java/WebsocketHandler.java
+++ server/src/main/java/WebsocketHandler.java
@@ -1,5 +1,18 @@
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+@WebSocket
 public class WebsocketHandler
 {
     // websocket back-end. Receiving messages from websocket facade. return the messages from it to send back to websocketFacade.
+    private Connection connection;
+    private final ConnectionManager connectionManager = new ConnectionManager();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message)
+    {
+
+    }
 
 }
