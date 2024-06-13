c0881008007687547587819b5061f1ec823c1e2c
Junha <sjh666@byu.edu>
2024-06-12 20:17:18 -0600

    create the Leave request class

---
+++ shared/src/main/java/websocket/commands/websocketRequests/Leave.java
@@ -0,0 +1,19 @@
+package websocket.commands.websocketRequests;
+
+import websocket.commands.UserGameCommand;
+
+public class Leave extends UserGameCommand {
+    private int gameID;
+    public Leave(String authToken, int gameID) {
+        super(authToken);
+        this.gameID = gameID;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
+
+    public void setGameID(int gameID) {
+        this.gameID = gameID;
+    }
+}
