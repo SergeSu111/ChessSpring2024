f4911bb1c300c51311741245b4d4715a9b4cdbe7
Junha <sjh666@byu.edu>
2024-06-14 21:49:39 -0600

    I just stuck on how to draw the board depends on the loading game message from server. I think I need to change my phase 5's BoardUI to passed in a chessboard. Because I just always set the board in phase 5 as resetBoard. But it should change.

--- client/src/main/java/ui/WebSocketFacade.java
+++ client/src/main/java/ui/WebSocketFacade.java
@@ -1,13 +1,18 @@
 package ui;
 
+import chess.ChessGame;
 import com.google.gson.Gson;
 import websocket.messages.ServerMessage;
+import websocket.messages.websocketResponse.ErrorWebsocket;
+import websocket.messages.websocketResponse.LoadGame;
 import websocket.messages.websocketResponse.Notification;
 
 import javax.websocket.*;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 
 public class WebSocketFacade extends Endpoint
 {
@@ -15,8 +20,10 @@ public class WebSocketFacade extends Endpoint
 
     NotificationHandler notificationHandler;
 
-    public WebSocketFacade(String url)
+    private ChessGame.TeamColor color;
+    public WebSocketFacade(String url, ChessGame.TeamColor color)
     {
+        this.color = color;
         try{
             url = url.replace("http", "ws");
             URI socketURI = new URI(url + "/ws");
@@ -33,23 +40,53 @@ public class WebSocketFacade extends Endpoint
                     ServerMessage serverMessage = new Gson().fromJson(message, ServerMessage.class);
                     switch (serverMessage.getServerMessageType())
                     {
-                        case ServerMessage.ServerMessageType.NOTIFICATION -> SendingNotification(message);
-                        case ServerMessage.ServerMessageType.ERROR ->
-                        case ServerMessage.ServerMessageType.LOAD_GAME ->
+                        case ServerMessage.ServerMessageType.NOTIFICATION -> SendingNotificationBack(message);
+                        case ServerMessage.ServerMessageType.ERROR -> SendingErrorBack(message);
+                        case ServerMessage.ServerMessageType.LOAD_GAME -> SendingLoadGameBack(message);
                     }
                 }
-
             });
         } catch (URISyntaxException | DeploymentException | IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void SendingNotification(String message)
+    private static void SendingNotificationBack(String message)
     {
         Gson gson = new Gson();
         Notification notification = gson.fromJson(message, Notification.class);
         System.out.println(notification.getMessage());
+    }
+
+    private static void SendingErrorBack(String message)
+    {
+        Gson gson = new Gson();
+        ErrorWebsocket error = gson.fromJson(message, ErrorWebsocket.class);
+        System.out.println(error.getErrorMessage());
+    }
+
+    private static void SendingLoadGameBack(String message)
+    {
+        Gson gson = new Gson();
+        LoadGame loadGame = gson.fromJson(message, LoadGame.class);
+        ChessGame game = loadGame.getGame();
+
+
+
+    }
+
+
+    public ChessGame.TeamColor getColor() {
+        return color;
+    }
+
+    public void setColor(ChessGame.TeamColor color) {
+        this.color = color;
+        if (color == ChessGame.TeamColor.WHITE)
+        {
+            var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+            BoardUI.callWhiteBoard(out);
+        }
     }
 
     @Override
