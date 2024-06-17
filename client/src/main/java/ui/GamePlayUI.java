e874adfae44db54729256d46d4fb1299c23a8f7d
Junha <sjh666@byu.edu>
2024-06-16 23:49:07 -0600

    create the GamePlayUI and give it a constructor and structure on it.

---
+++ client/src/main/java/ui/GamePlayUI.java
@@ -0,0 +1,67 @@
+package ui;
+
+import chess.ChessGame;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class GamePlayUI
+{
+    private static final PrintStream OUT = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private WebSocketFacade webSocketFacade = new WebSocketFacade("http://localhost:8080", ChessGame.TeamColor.WHITE);
+
+    private PostLogin postLogin;
+
+    private String authToken;
+    public GamePlayUI(String serverUrl, String authToken)
+    {
+        ServerFacade serverfacade = new ServerFacade(serverUrl);
+        this.authToken = authToken;
+    }
+
+    public void run()
+    {
+        OUT.println();
+        OUT.println("Welcome to your chess game. Please make your choice and enjoy the game.");
+
+        OUT.println();
+        OUT.println(help());
+        String input = SCANNER.nextLine();
+        while (!Objects.equals(input, "Quit"))
+        {
+            this.eval(input);
+            input = SCANNER.nextLine();
+        }
+    }
+
+    public void eval(String input)
+    {
+        switch (input)
+        {
+            case "Redraw Chess Board" -> Redraw();
+            case "Leave" -> leave();
+            case "Make Move" -> MakeMove();
+            case "Resign" -> Resign();
+            case "Highlight Legal Moves" ->HightLight();
+            case "Help" -> OUT.println(help());
+            default -> OUT.println(help());
+        }
+    }
+
+    public static String help()
+    {
+        return """
+               Redraw Chess Board -- Redraws the chess board upon the user’s request.
+               Leave -- Removes the user from the game. The client transitions back to the Post-Login UI.
+               Make Move -- Allow the user to input what move they want to make to make board update.
+               Resign -- Resign the game.
+               Highlight Legal Moves -- Allows the user to input the piece for which they want to highlight legal moves.
+               Help - With possible commands.
+                """;
+    }
+}
